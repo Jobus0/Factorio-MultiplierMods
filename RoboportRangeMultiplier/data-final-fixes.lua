0b427d12eda962dbecc7b7ab43cb749d1f733328
Jobus <johan.tylon@gmail.com>
2024-10-24 16:21:26 +0200

    Add RoboportRangeMultiplier

---
+++ RoboportRangeMultiplier/data-final-fixes.lua
@@ -0,0 +1,4 @@
+for _, prot in pairs(data.raw['roboport']) do
+    prot.logistics_radius = math.max(math.floor(prot.logistics_radius*settings.startup['RoboportRangeMultiplier-logistics-range-factor'].value + 0.5), 0)
+    prot.construction_radius = math.max(math.floor(prot.construction_radius*settings.startup['RoboportRangeMultiplier-construction-range-factor'].value + 0.5), 0)
+end
---
+++ RoboportRangeMultiplier/settings.lua
@@ -0,0 +1,14 @@
+data:extend({
+  {
+    type = "double-setting",
+    name = "RoboportRangeMultiplier-logistics-range-factor",
+    setting_type = "startup",
+    default_value = 1.0
+  },
+  {
+    type = "double-setting",
+    name = "RoboportRangeMultiplier-construction-range-factor",
+    setting_type = "startup",
+    default_value = 1.0
+  }
+})
