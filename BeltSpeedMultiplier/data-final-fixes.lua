d194bed25dad0dbd2884461cfac1eaf193a4e09e
Jobus <johan.tylon@gmail.com>
2024-10-26 00:32:24 +0200

    BeltSpeedMultiplier: Support all mods that add loaders, lane splitters, or linked belts

--- BeltSpeedMultiplier/data-final-fixes.lua
+++ BeltSpeedMultiplier/data-final-fixes.lua
@@ -1,16 +1,18 @@
 local multiplier = settings.startup['BeltSpeedMultiplier-speed-factor'].value
 
-for _, type_name in pairs({'transport-belt', 'underground-belt', 'splitter'}) do
+local transport_belt_connectables = {
+    "transport-belt",
+    "underground-belt",
+    "splitter",
+    "loader",
+    "loader-1x1",
+    "linked-belt",
+    "lane-splitter",
+}
+
+for _, type_name in pairs(transport_belt_connectables) do
 
     local type = data.raw[type_name]
-
-    for _, prot in pairs(type) do
-        prot.speed = prot.speed*multiplier
-    end
-end
-
-if (mods["aai-loaders"]) then
-    local type = data.raw['loader-1x1']
 
     for _, prot in pairs(type) do
         prot.speed = prot.speed*multiplier
