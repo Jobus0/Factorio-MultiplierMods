5c8f58f0505698014223a2ae32f844aa262ceddc
Jobus <johan.tylon@gmail.com>
2024-10-28 16:39:02 +0100

    ElectricPoleRangeMultiplier: Uncap maximum supply area from 64x64 to 128x128

--- ElectricPoleRangeMultiplier/data-final-fixes.lua
+++ ElectricPoleRangeMultiplier/data-final-fixes.lua
@@ -1,4 +1,4 @@
 for _, prot in pairs(data.raw['electric-pole']) do
-    prot.supply_area_distance = math.min(math.floor(prot.supply_area_distance*settings.startup['ElectricPoleRangeMultiplier-range-factor'].value + 0.5), 32)
+    prot.supply_area_distance = math.min(math.floor(prot.supply_area_distance*settings.startup['ElectricPoleRangeMultiplier-range-factor'].value + 0.5), 64)
     prot.maximum_wire_distance = math.min(math.floor(prot.maximum_wire_distance*settings.startup['ElectricPoleRangeMultiplier-range-factor'].value + 0.5), 64)
 end
