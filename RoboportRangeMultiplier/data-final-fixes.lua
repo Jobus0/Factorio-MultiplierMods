5283b00bad29b2fde252e43a789e6987d233a08a
Jobus <johan.tylon@gmail.com>
2024-10-31 16:13:28 +0100

    RoboportRangeMultiplier: Add multiplier for personal roboports

--- RoboportRangeMultiplier/data-final-fixes.lua
+++ RoboportRangeMultiplier/data-final-fixes.lua
@@ -2,3 +2,7 @@ for _, prot in pairs(data.raw['roboport']) do
     prot.logistics_radius = math.max(math.floor(prot.logistics_radius*settings.startup['RoboportRangeMultiplier-logistics-range-factor'].value + 0.5), 0)
     prot.construction_radius = math.max(math.floor(prot.construction_radius*settings.startup['RoboportRangeMultiplier-construction-range-factor'].value + 0.5), 0)
 end
+
+for _, prot in pairs(data.raw['roboport-equipment']) do
+    prot.construction_radius = math.max(math.floor(prot.construction_radius*settings.startup['RoboportRangeMultiplier-personal-construction-range-factor'].value + 0.5), 0)
+end
--- RoboportRangeMultiplier/settings.lua
+++ RoboportRangeMultiplier/settings.lua
@@ -10,5 +10,11 @@ data:extend({
     name = "RoboportRangeMultiplier-construction-range-factor",
     setting_type = "startup",
     default_value = 1.0
+  },
+  {
+    type = "double-setting",
+    name = "RoboportRangeMultiplier-personal-construction-range-factor",
+    setting_type = "startup",
+    default_value = 1.0
   }
 })
