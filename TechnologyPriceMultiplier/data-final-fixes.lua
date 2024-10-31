d4d3af4dff08b1741a131adf02639bd2ace78233
Jobus <johan.tylon@gmail.com>
2024-10-31 14:16:04 +0100

    TechnologyPriceMultiplier: Add exponential growth multiplier

--- TechnologyPriceMultiplier/data-final-fixes.lua
+++ TechnologyPriceMultiplier/data-final-fixes.lua
@@ -1,1 +1,25 @@
-require('prototypes.technology.technology')
+local technologies = data.raw.technology
+
+local price_factor = settings.startup["TechnologyPriceMultiplier-price-factor"].value
+local price_exponent = settings.startup["TechnologyPriceMultiplier-price-exponent-factor"].value
+
+local exponent_base_pattern = "^%d*%.?%d+"
+
+price_exponent = math.max(price_exponent, 0)
+
+for _, technology in pairs(technologies) do
+    if (technology.unit) then
+        if (technology.unit.count ~= nil) then
+            technology.unit.count = math.max(math.ceil(technology.unit.count*price_factor), 1)
+        elseif (technology.unit.count_formula) then
+            if (price_exponent ~= 1) then
+                local base = string.match(technology.unit.count_formula, exponent_base_pattern)
+                if (base ~= nil) then
+                    local replacement = "((" .. base .. " - 1)*" .. price_exponent .. " + 1)"
+                    technology.unit.count_formula = technology.unit.count_formula:gsub(exponent_base_pattern, replacement, 1)
+                end
+            end
+            technology.unit.count_formula = '(' .. technology.unit.count_formula .. ')*' .. price_factor
+        end
+    end
+end
--- TechnologyPriceMultiplier/settings.lua
+++ TechnologyPriceMultiplier/settings.lua
@@ -4,5 +4,11 @@ data:extend({
     name = "TechnologyPriceMultiplier-price-factor",
     setting_type = "startup",
     default_value = 1.0
+  },
+  {
+    type = "double-setting",
+    name = "TechnologyPriceMultiplier-price-exponent-factor",
+    setting_type = "startup",
+    default_value = 1.0
   }
 })
