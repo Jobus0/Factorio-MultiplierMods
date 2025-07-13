8f45f140c9f05256edbd5fe810048c8cadc32ae7
Jobus <johan.tylon@gmail.com>
2025-07-13 13:53:32 +0200

    ElectricPoleRangeMultiplier: Refactor to reuse fetched setting

--- ElectricPoleRangeMultiplier/data-final-fixes.lua
+++ ElectricPoleRangeMultiplier/data-final-fixes.lua
@@ -1,4 +1,5 @@
+local factor = settings.startup['ElectricPoleRangeMultiplier-range-factor'].value
 for _, prot in pairs(data.raw['electric-pole']) do
-    prot.supply_area_distance = math.min(math.floor(prot.supply_area_distance*settings.startup['ElectricPoleRangeMultiplier-range-factor'].value + 0.5), 64)
-    prot.maximum_wire_distance = math.min(math.floor(prot.maximum_wire_distance*settings.startup['ElectricPoleRangeMultiplier-range-factor'].value + 0.5), 64)
+    prot.supply_area_distance = math.min(math.floor(prot.supply_area_distance*factor + 0.5), 64)
+    prot.maximum_wire_distance = math.min(math.floor(prot.maximum_wire_distance*factor + 0.5), 64)
 end
