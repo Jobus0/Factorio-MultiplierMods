c83b536e3efbf6f6751b5d50ee720f78c1d5b3e2
Jobus <johan.tylon@gmail.com>
2025-07-27 12:26:34 +0200

    Add multiplier for science pack tier-based price scaling

--- TechnologyPriceMultiplier/data-final-fixes.lua
+++ TechnologyPriceMultiplier/data-final-fixes.lua
@@ -2,15 +2,43 @@ local technologies = data.raw.technology
 
 local price_factor = settings.startup["TechnologyPriceMultiplier-price-factor"].value
 local price_exponent = settings.startup["TechnologyPriceMultiplier-price-exponent-factor"].value
+local price_tier_scaling = settings.startup["TechnologyPriceMultiplier-price-tier-scaling-factor"].value
 
 local exponent_base_pattern = "^%d*%.?%d+"
 
-price_exponent = math.max(price_exponent, 0)
+function count_prerequisite_tier(technology)
+    if technology.prerequisites == nil or #technology.prerequisites == 0 then
+        return 0
+    end
+
+    local max_prereq_count = 0
+    for _, prereq_key in ipairs(technology.prerequisites) do
+        local prereq_tech = technologies[prereq_key]
+        if prereq_tech then
+            local current_prereq_count = count_prerequisite_tier(prereq_tech)
+
+            if prereq_tech.unit ~= nil and prereq_tech.essential then
+                current_prereq_count = current_prereq_count + 1
+            end
+
+            if current_prereq_count > max_prereq_count then
+                max_prereq_count = current_prereq_count
+            end
+        end
+    end
+
+    return max_prereq_count
+end
 
 for _, technology in pairs(technologies) do
     if (technology.unit) then
+        local tier = 0
+        if (price_tier_scaling ~= 1) then
+            tier = count_prerequisite_tier(technology)
+        end
+
         if (technology.unit.count ~= nil) then
-            technology.unit.count = math.max(math.ceil(technology.unit.count*price_factor), 1)
+            technology.unit.count = math.max(math.ceil(technology.unit.count*price_factor*price_tier_scaling^tier), 1)
         elseif (technology.unit.count_formula) then
             if (price_exponent ~= 1) then
                 local base = string.match(technology.unit.count_formula, exponent_base_pattern)
@@ -19,7 +47,7 @@ for _, technology in pairs(technologies) do
                     technology.unit.count_formula = technology.unit.count_formula:gsub(exponent_base_pattern, replacement, 1)
                 end
             end
-            technology.unit.count_formula = '(' .. technology.unit.count_formula .. ')*' .. price_factor
+            technology.unit.count_formula = '(' .. technology.unit.count_formula .. ')*' .. price_factor .. "*" .. price_tier_scaling .. "^" .. tier
         end
     end
 end
--- TechnologyPriceMultiplier/settings.lua
+++ TechnologyPriceMultiplier/settings.lua
@@ -10,5 +10,12 @@ data:extend({
     name = "TechnologyPriceMultiplier-price-exponent-factor",
     setting_type = "startup",
     default_value = 1.0
+  },
+  {
+    type = "double-setting",
+    name = "TechnologyPriceMultiplier-price-tier-scaling-factor",
+    setting_type = "startup",
+    default_value = 1.0,
+    minimum_value = 0.1
   }
 })
