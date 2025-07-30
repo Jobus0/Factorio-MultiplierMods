ad9c7388bbb0a58c4c940639000bfc76cb2a9144
Jobus <johan.tylon@gmail.com>
2025-07-30 02:33:28 +0200

    TechnologyPriceMultiplier: Add recursion memoization

--- TechnologyPriceMultiplier/data-final-fixes.lua
+++ TechnologyPriceMultiplier/data-final-fixes.lua
@@ -5,28 +5,32 @@ local price_exponent = settings.startup["TechnologyPriceMultiplier-price-exponen
 local price_tier_scaling = settings.startup["TechnologyPriceMultiplier-price-tier-scaling-factor"].value
 
 local exponent_base_pattern = "^%d*%.?%d+"
+local tier_cache = {}
 
 function count_prerequisite_tier(technology)
-    if technology.prerequisites == nil or #technology.prerequisites == 0 then
-        return 0
+    if tier_cache[technology.name] then
+        return tier_cache[technology.name]
     end
 
     local max_prereq_count = 0
-    for _, prereq_key in ipairs(technology.prerequisites) do
-        local prereq_tech = technologies[prereq_key]
-        if prereq_tech then
-            local current_prereq_count = count_prerequisite_tier(prereq_tech)
+    if technology.prerequisites and #technology.prerequisites > 0 then
+        for _, prereq_key in ipairs(technology.prerequisites) do
+            local prereq_tech = technologies[prereq_key]
+            if prereq_tech and prereq_tech ~= technology.name then
+                local current_prereq_count = count_prerequisite_tier(prereq_tech)
 
-            if prereq_tech.unit ~= nil and prereq_tech.essential then
-                current_prereq_count = current_prereq_count + 1
-            end
+                if prereq_tech.essential and prereq_tech.unit ~= nil then
+                    current_prereq_count = current_prereq_count + 1
+                end
 
-            if current_prereq_count > max_prereq_count then
-                max_prereq_count = current_prereq_count
+                if current_prereq_count > max_prereq_count then
+                    max_prereq_count = current_prereq_count
+                end
             end
         end
     end
-
+    
+    tier_cache[technology.name] = max_prereq_count
     return max_prereq_count
 end
 
@@ -39,7 +43,7 @@ for _, technology in pairs(technologies) do
 
         if (technology.unit.count ~= nil) then
             technology.unit.count = math.max(math.ceil(technology.unit.count*price_factor*price_tier_scaling^tier), 1)
-        elseif (technology.unit.count_formula) then
+        elseif (technology.unit.count_formula ~= nil) then
             if (price_exponent ~= 1) then
                 local base = string.match(technology.unit.count_formula, exponent_base_pattern)
                 if (base ~= nil) then
