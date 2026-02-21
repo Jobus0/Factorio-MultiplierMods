223cbadb9dee6d2618fc7d62e2cece2c9ee0c091
Jobus <johan.tylon@gmail.com>
2026-02-21 23:26:39 +0100

    ElectricPoleRangeMultiplier: Enhance electric pole settings with maximum gap option and improved distance calculations

--- ElectricPoleRangeMultiplier/data-final-fixes.lua
+++ ElectricPoleRangeMultiplier/data-final-fixes.lua
@@ -1,5 +1,11 @@
 local factor = settings.startup['ElectricPoleRangeMultiplier-range-factor'].value
+
 for _, prot in pairs(data.raw['electric-pole']) do
-    prot.supply_area_distance = math.min(math.floor(prot.supply_area_distance*factor + 0.5), 64)
-    prot.maximum_wire_distance = math.min(math.floor(prot.maximum_wire_distance*factor + 0.5), 64)
+    prot.supply_area_distance = math.min(math.floor(prot.supply_area_distance*factor*2)/2, 64)
+    
+    maximum_wire_distance = math.min(math.floor(prot.maximum_wire_distance*factor*2)/2, 64)
+    if (prot.supply_area_distance*2)/maximum_wire_distance > 0.5 then
+        maximum_wire_distance = math.min(maximum_wire_distance, prot.supply_area_distance*2 + settings.startup['ElectricPoleRangeMultiplier-maximum-gap'].value)
+    end
+    prot.maximum_wire_distance = math.min(maximum_wire_distance, 64)
 end
--- ElectricPoleRangeMultiplier/settings.lua
+++ ElectricPoleRangeMultiplier/settings.lua
@@ -3,6 +3,16 @@ data:extend({
     type = "double-setting",
     name = "ElectricPoleRangeMultiplier-range-factor",
     setting_type = "startup",
-    default_value = 1.0
+    order = "a",
+    default_value = 1.0,
+    minimum = 0.0
+  },
+  {
+    type = "double-setting",
+    name = "ElectricPoleRangeMultiplier-maximum-gap",
+    setting_type = "startup",
+    order = "b",
+    default_value = 2.5,
+    minimum = 0.0
   }
 })
